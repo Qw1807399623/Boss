8f616d52409d8e1bf250a7714b68cc4a3d51142b
Aaron <lzl@lzl.com>
2019-08-16 20:02:04 +0800

    Aaeon

--- boss/src/main.js
+++ boss/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 //mint-ui
 import MintUi from "mint-ui"
 Vue.use(MintUi)
--- boss/src/router.js
+++ boss/src/router.js
@@ -3,11 +3,13 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Reg from './views/Reg.vue'
+import Details from './views/Details.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {path:"/Details",component:Details},
     {path:"/Login",component:Login},
     {path:"/Reg",component:Reg},
     {path: '/',name: 'home',component: Home},
